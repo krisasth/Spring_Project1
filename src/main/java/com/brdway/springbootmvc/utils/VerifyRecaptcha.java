f28a600278fd3d2a74b64ae56d804eb42aab9055
krisasth <krishashrestha2059@gmail.com>
2025-09-18 20:21:59 +0545

    added recaptcha

--- src/main/java/com/brdway/springbootmvc/utils/VerifyRecaptcha.java
+++ src/main/java/com/brdway/springbootmvc/utils/VerifyRecaptcha.java
@@ -15,7 +15,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class VerifyRecaptcha {
 
 	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
-	public static final String secret = "6Ld7_sorAAAAALMnAvk1fmUy3oVCJf-fBtGSSHY0";
+	public static final String secret = "6LfYs80rAAAAAGTMYfV5gfKdfpl2n9Xy9TpjQq8g";
 	private final static String USER_AGENT = "Mozilla/5.0";
 
 	public static boolean verify(String gRecaptchaResponse) throws IOException {
